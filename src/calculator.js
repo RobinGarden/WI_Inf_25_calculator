d0660ea9662c60d84c88c31edbe11bc50fffdd6f
Jonas Gabel <j.gabel@sap.com>
2025-10-24 14:37:08 +0200

    feat: New function to return the number from the input

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,9 @@
 function add(a, b) { return a + b; }
 function sub(a, b) { return a - b; }
 function mul(a, b) { return a * b; }
+function number(a) { return a;}
 function div(a, b) {
 if (b === 0) throw new Error('DivideByZeroException');
 return a / b;
 }
-module.exports = { add, sub, mul, div};
+module.exports = { add, sub, mul, div, number};
